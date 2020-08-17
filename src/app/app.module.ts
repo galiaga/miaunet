c5daea6c9f0ef5387dbff4c7f198439bee36191f
galiaga <galiaga@gmail.com>
2020-08-17 14:15:24 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { CatsComponent } from './components/cats/cats.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CatsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, routing],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { CatsComponent } from './components/cats/cats.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'sobre-miaunet', component: AboutComponent },
+  { path: 'gatos', component: CatsComponent },
+  { path: 'crear-gato', component: CreateComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', component: ErrorComponent },
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
